1bae695d91b3538c25b107394ce980e3849f0d0f
jplasch <j.plasch@zoho.com>
2020-05-19 19:55:08 -0700

    updating...

--- src/repository/reimbursement-data-update.ts
+++ src/repository/reimbursement-data-update.ts
@@ -4,15 +4,26 @@ import { connectionPool } from ".";
 
 
 
-export async function updateReimbursement(reimbursementId: number, author: string, amount: number,
-    dateSubmitted: number, dateResolved: number, description: string, resolver: string,
+export async function updateReimbursement(reimbursementId: number, author: number, amount: number,
+    dateSubmitted: number, dateResolved: number, description: string, resolver: number,
     status: number, type: number): Promise<Reimbursement[]> {
     let client : PoolClient;
     client = await connectionPool.connect();
     try {
         let result : QueryResult = await client.query(
+            `INSERT INTO reimbursements
+            VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9);`, [reimbursementId, author, amount, dateSubmitted,
+                dateResolved, description, resolver, status, type]
+        );
+        return result.rows.map((u)=>{
+            return new Reimbursement(u.reimbursementId, u.author, u.amount, u.dateSubmitted,
+                u.dateResolved, u.description, u.resolver, u.status, u.type);
+        });
+    } catch (e) {
+        let result : QueryResult = await client.query(
             `UPDATE reimbursements
-            SET author = $2,
+            SET reimbursementId = $1,
+                author = $2,
                 amount = $3,
                 dateSubmitted = $4,
                 dateResolved = $5,
@@ -27,8 +38,6 @@ export async function updateReimbursement(reimbursementId: number, author: strin
             return new Reimbursement(u.reimbursementId, u.author, u.amount, u.dateSubmitted,
                 u.dateResolved, u.description, u.resolver, u.status, u.type);
         });
-    } catch (e) {
-        throw new Error(`Failed to update user: ${e.message}`);
     } finally {
         client && client.release();
     }
