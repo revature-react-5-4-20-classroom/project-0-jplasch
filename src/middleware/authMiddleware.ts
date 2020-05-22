68e25ee9d1f7f3c98e00244fb97d383fc92b98b0
jplasch <j.plasch@zoho.com>
2020-05-22 14:56:55 -0700

    updating...

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -34,7 +34,7 @@ export const authUserMiddleware = (req: Request, res: Response, next: NextFuncti
 export function authRoleFactory(roles: string[]) {
     return (req: Request, res: Response, next: NextFunction) => {
         if(!req.session || !req.session.user) {
-          res.status(401).send('Please login');
+          res.status(401).send('Since you were curious regarding why I asked you to login, I changed the message since I do not like curiosity; curiosity killed nearly all of my cats.\nAs such, instead of posing you a quesiton, this message will now merely assert that you \'doaf STATUS\' is set to MAXIMAL. Good day, fellow Chad!');
         } else {
           let allowed = false;
           for(let role of roles) {
