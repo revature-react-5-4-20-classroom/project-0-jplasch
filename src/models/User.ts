4c0a0be17b91578c8fc2d4cc33919359ba25211b
jplasch <j.plasch@zoho.com>
2020-06-08 00:30:00 -0700

    changed User role to string

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,6 @@
 // User model keeps track of user information
 
-import { Role } from "./Role";
+// import { Role } from "./Role";
 
 export class User {
     userId: number; // primary key
@@ -9,10 +9,10 @@ export class User {
     firstName: string; // not null
     lastName: string; // not null
     email: string; // not null
-    role: Role; // not null
+    role: string; // not null
 
     constructor(userId: number, username: string, password: string,
-        firstName: string, lastName: string, email: string, role: Role){
+        firstName: string, lastName: string, email: string, role: string){
         this.userId = userId;
         this.username = username;
         this.password = password;
